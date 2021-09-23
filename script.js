5f4a42fe786a2300cb2e1613e63eecdc3fee96c6
TZ <tz116917@gmail.com>
2021-09-22 19:47:16 -0700

    added password generator, met all criteria

--- script.js
+++ script.js
@@ -1,6 +1,94 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
+var arrayNum = ["0","1","2","3","4","5","6","7","8","9"]
+var arrayLet = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"]
+var arraySpe = ["\!","\"","\#","\$","\%","\&","\'","\(","\)","\*","\+","\,","\-","\.","\/","\:","\;","\<","\=","\>","\?","\@","\[","\\","\]","\^","\_","\`","\{","\|","\}","\~"]
+
+
+function generatePassword() {
+
+  var promptLength = prompt("How long should the password be? (8-128 chatacters)");
+
+  if (promptLength == null) {
+    return null;
+  }
+
+  var passLength = parseInt(promptLength);
+
+  if ((typeof(passLength)!= 'number')||(passLength % 1 != 0)) {
+    alert("Please enter an integer between 8-128")
+
+  }
+
+  else if (passLength<8) {
+
+    alert("Minimum length is 8 characters.")
+  }
+
+  else if (passLength>128) {
+
+    alert("Maximum length is 128 characters.")
+  }
+  
+
+  else {
+
+    var confirmNum = confirm("Click OK to confirm including numbers.");
+
+    var confirmLower = confirm("Click OK to confirm including lowercase letters.");
+
+    var confirmUpper = confirm("Click OK to confirm including uppercase letters.");
+
+    var confirmSpecial = confirm("Click OK to confirm including special characters.");
+    
+  }
+
+  var pass = "";
+
+  var pool = [];
+
+  if (confirmNum == true) {
+    pool.push("0","1","2","3","4","5","6","7","8","9")
+
+  }
+
+  if (confirmLower == true) {
+    pool.push("a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z")
+    
+  }
+
+  if (confirmUpper == true) {
+    pool.push("A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z")
+
+    
+  }
+
+  if (confirmSpecial == true) {
+    pool.push("\!","\"","\#","\$","\%","\&","\'","\(","\)","\*","\+","\,","\-","\.","\/","\:","\;","\<","\=","\>","\?","\@","\[","\\","\]","\^","\_","\`","\{","\|","\}","\~")
+
+    
+  }
+
+  if ((confirmNum||confirmLower||confirmSpecial||confirmUpper) == false) {
+    alert("Must select at least one character type.")
+    return null;
+  }
+
+
+  for (let i=0; i<passLength; i++) {
+    let indexPool = Math.floor(Math.random()*pool.length);
+    pass=pass.concat(pool[indexPool]);
+
+  }
+
+  return pass;
+  
+  
+
+}
+  
+
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();

76010579cfa629d70ad2c035ebd5af40e638912f
TZ <tz116917@gmail.com>
2021-09-23 20:13:02 -0700

    js comments and readme update

--- README.md
+++ README.md
@@ -1,1 +1,62 @@
-# PasswordGenerator
+# Password Generator
+
+Given html and css files, this project aims to develop an algorithm in javascript that randomly generates a password between 8-128 characters and meets criteria for character types selected by the user via confirms. 
+​
+## Getting Started
+​
+To open the deployed site, click [here](https://tonyzyt9947.github.io/PasswordGenerator/).
+
+To view the html code, open index.html.
+
+To view the css code, open style.css.
+
+To view the javascript code, open script.js.
+​
+<br/>
+
+### Prerequisites
+​
+The site should be able to run on any web browser.
+<br/>
+### Installing
+
+Click [here](https://www.google.com/chrome/) to install google chrome to open the site.
+
+<br/>
+
+## Built With
+
+* [HTML](https://developer.mozilla.org/en-US/docs/Web/HTML)
+* [CSS](https://developer.mozilla.org/en-US/docs/Web/CSS)
+* [Javascript](https://developer.mozilla.org/en-US/docs/Web/Javascript)
+<br/><br/>
+## Deployed Link
+
+* [See Live Site](https://tonyzyt9947.github.io/PasswordGenerator/)
+​
+​<br/><br/>
+## Application Features and Code
+
+View javascript comments.
+
+## Demonstration
+
+![Password Generator Demonstration](demo.gif)
+
+## Authors
+
+* **Tony Zhang** 
+- [Link to Portfolio Site](https://tonyzyt9947.github.io/PersonalPortfolio/)
+- [Link to Github](https://github.com/Tonyzyt9947)
+- [Link to LinkedIn](https://www.linkedin.com/in/tony-zhang-61670421b/)
+​
+<br/><br/>
+
+## License
+​
+This project is licensed under the MIT License 
+​
+<br/><br/>
+## Acknowledgments
+* Inspiration: Lecture by Manuel Matuzovic, _"Writing CSS with Accessibility in Mind"_ 2017. [Link](https://www.youtube.com/watch?v=EOiC2M47GBY&t=503s)
+* Lighthouse chrome extension. [Link](https://developers.google.com/web/tools/lighthouse)
--- script.js
+++ script.js
@@ -1,37 +1,38 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
-var arrayNum = ["0","1","2","3","4","5","6","7","8","9"]
-var arrayLet = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"]
-var arraySpe = ["\!","\"","\#","\$","\%","\&","\'","\(","\)","\*","\+","\,","\-","\.","\/","\:","\;","\<","\=","\>","\?","\@","\[","\\","\]","\^","\_","\`","\{","\|","\}","\~"]
-
-
+// main function
 function generatePassword() {
 
   var promptLength = prompt("How long should the password be? (8-128 chatacters)");
 
+  // Error check: if user cancels/refuse to enter input.
   if (promptLength == null) {
     return null;
   }
 
+  // Turns string input into integers
   var passLength = parseInt(promptLength);
 
+  // Error check: if user's input is not an integer.
   if ((typeof(passLength)!= 'number')||(passLength % 1 != 0)) {
     alert("Please enter an integer between 8-128")
 
   }
 
+  // Error check: if selected length is less than 8.
   else if (passLength<8) {
 
     alert("Minimum length is 8 characters.")
   }
 
+  // Error check: if selected length is longer than 128.
   else if (passLength>128) {
 
     alert("Maximum length is 128 characters.")
   }
   
-
+  // Let user select character types through confirms.
   else {
 
     var confirmNum = confirm("Click OK to confirm including numbers.");
@@ -44,10 +45,12 @@ function generatePassword() {
     
   }
 
+  // PLaceholder variables for password and set of avaliable character types.
   var pass = "";
 
   var pool = [];
 
+  // Adds selected character types into pool.
   if (confirmNum == true) {
     pool.push("0","1","2","3","4","5","6","7","8","9")
 
@@ -70,12 +73,13 @@ function generatePassword() {
     
   }
 
+  // Error check: if user did not choose any character types.
   if ((confirmNum||confirmLower||confirmSpecial||confirmUpper) == false) {
     alert("Must select at least one character type.")
     return null;
   }
 
-
+  // randomly selected a character from character pool and adds it to password until reached password length.
   for (let i=0; i<passLength; i++) {
     let indexPool = Math.floor(Math.random()*pool.length);
     pass=pass.concat(pool[indexPool]);
